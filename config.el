5e35bfd69c6dbee0d8ee18c4f85da82202347145
Zachary King <zak@delfina.com>
2022-12-05 08:35:14 -0800

    🐥

--- config.el
+++ config.el
@@ -1,8 +1,6 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
+;; Remember, you do not need to run 'doom sync' after modifying this file!
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
@@ -21,27 +19,30 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 
+;;; Theme
+;;
 (setq doom-font "JetBrains Mono:weight=Light:pixelsize=14")
-
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+;; https://github.com/d12frosted/homebrew-emacs-plus
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (setq doom-theme 'doom-one-light) (load-theme 'doom-one-light t))
+    ('dark (setq doom-theme 'doom-one) (load-theme 'doom-one t))))
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+
+;;; Basic settings
+
 (setq display-line-numbers-type nil)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+;;; Custom keybindings
 
+(global-set-key (kbd "s-<escape>") #'delete-other-windows)
 
+;;; Notes
+;;
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
@@ -75,12 +76,3 @@
 ;; they are implemented.
 
 
-;; https://github.com/d12frosted/homebrew-emacs-plus
-(defun my/apply-theme (appearance)
-  "Load theme, taking current system APPEARANCE into consideration."
-  (mapc #'disable-theme custom-enabled-themes)
-  (pcase appearance
-    ('light (setq doom-theme 'doom-one-light) (load-theme 'doom-one-light t))
-    ('dark (setq doom-theme 'doom-one) (load-theme 'doom-one t))))
-
-(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
